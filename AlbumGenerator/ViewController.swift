1932da11f0145621710643b4098c7b33d468d761
Chris Eccleston <3098878+c3ccl3ston@users.noreply.github.com>
2021-02-18 12:52:20 -0500

    Initial commit of AlbumGenerator demo app

--- AlbumGenerator/ViewController.swift
+++ AlbumGenerator/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var albumImage: UIImageView!
+    
+    @IBAction func albumTapped(_ sender: Any) {
+        loadAlbumImage()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadAlbumImage()
     }
-
-
+    
+    func loadAlbumImage() {
+        let imageUrlString = "https://picsum.photos/500/"
+        guard let imageUrl:URL = URL(string: imageUrlString) else {
+            return
+        }
+        
+        DispatchQueue.global().async { [weak self, weak weakImage = albumImage] in
+            if let imageData = try? Data(contentsOf: imageUrl) {
+                if let image = UIImage(data: imageData) {
+                    DispatchQueue.main.async {
+                        weakImage?.image = nil
+                        weakImage?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
-
