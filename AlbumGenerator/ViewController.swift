1ebc85e250a04c7e921feb25845dd57914dca3a2
Chris Eccleston <3098878+c3ccl3ston@users.noreply.github.com>
2021-02-18 10:20:43 -0500

    Initial Commit

---
+++ AlbumGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumGenerator
+//
+//  Created by Chris Eccleston on 2/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
